5e9e1a746f8bfd8e89f09c9461cadcfecfa2270b
David Rochholz <mail@davidrochholz.de>
2022-01-04 03:53:47 +0100

    changed retrieval from log to user

--- handlers/handleRetrieveUserLog.js
+++ handlers/handleRetrieveUserLog.js
@@ -1,15 +1,21 @@
 const logCollection = require('../collections/LogCollection');
+const userCollection = require('../collections/UserCollection');
 
 function handleRetrieveUserLog(req, res, next) {
 
     const userId = req.params['_id'];
     const log = logCollection.findLogByUserId(userId)[0];
+    const user = { ...userCollection.findById(userId)[0] };
 
     const from = req.query['from'];
     const to = req.query['to'];
     const limit = req.query['limit'];
 
-    res.json(log.retrieve({ from, to, limit }));
+    log = log.retrieve({ from, to, limit });
+    user.count = log.length;
+    user.log = log;
+
+    res.json(user);
 }
 
 module.exports = handleRetrieveUserLog;
