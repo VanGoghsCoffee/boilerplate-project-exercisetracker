261d6f185a3ff9aaed8467d3a85aa1f96d44d231
David Rochholz <mail@davidrochholz.de>
2022-01-04 04:10:28 +0100

    added logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,6 @@
 function logRequests(req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.body} - ${req.ip}`);
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    console.log(req.body);
     next();
 }
 
